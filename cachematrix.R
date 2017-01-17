279449a2fd9747ea1905aff0b5f6841cdad2ee6b
Rodrigo <rods.kuro@gmail.com>
2017-01-17 21:35:47 -0200

    added cachematrix.R and testing_cachematrix.ipynb

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Function 1: makeCacheMatrix
+# returns an object that has a list with 4 functions.
+# These functions get and set values.
+# Example: matrix1 <- makeCacheMatrix(matrix(c(1,2,3,4),nrow=2))
+# To get the matrix, you can call matrix1$get()
+# To get the inverse:  matrix1$getinverse() 
+##########
+# Function 2: cacheSolve
+# returns the inverse of the argument x
+# If x has a NULL inverse, then calculates it.
+# If the inverse is not NULL, then just return it.
+# Example: cacheSolve(matrix1)
+# If you are calling it for the first time, it will
+# print 'Calculating the inverse'
+##########
 
-## Write a short comment describing this function
 
+#Creates an object that is a list of 4 functions.
+#Very similar to 'makeVector' from the example
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	
+	set <- function(matr) {
+		x <<- matr
+		inverse <<- NULL
+	}
+	
+	get <- function() x
+	
+	setinverse <- function(inv) inverse <<- inv
+	
+	getinverse <- function() inverse
+	
+	#Return the list with the functions
+	list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+#Calculates the inverse if it's NULL
+#Otherwise, use the function getinverse: no calculation needed.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+		inverse <- x$getinverse()
+		
+		if(!is.null(inverse))
+			return(inverse)
+		
+		matr <- x$get()
+		message("Calculating the inverse")
+		inverse <- solve(matr)
+		x$setinverse(inverse)
+		return(inverse)
 }
